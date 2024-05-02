1d46077fa5c7580673d2ab6ed0b73873664d0be8
Никита <solomon.www@mail.ru>
2024-05-02 15:00:16 +0300

    some fix and comments

--- contracts/PreSaleContract.sol
+++ contracts/PreSaleContract.sol
@@ -5,17 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PreSaleContract is Ownable {
-    IERC20 public usdtToken;
-    IERC20 public preSaleToken;
+    IERC20 public usdtToken; // USDT token contract
+    IERC20 public preSaleToken; // preSale token contract
 
-    uint256 public startTimestamp;
-    uint256 public endTimestamp;
-    uint256 public goalUsdt;
+    uint256 public startTimestamp; // Start time of the funding (UNIX timestamp)
+    uint256 public endTimestamp; // End time of the funding (UNIX timestamp)
+    uint256 public goalUsdt; // Goal in USDT
 
-    bool public fundingComplete = false;
-    bool public tokensDeposited = false;
-    uint256 public totalUsdtCollected = 0;
-    mapping(address => uint256) public contributions;
+    bool public fundingComplete = false; // Flag indicating the funding is complete
+    bool public tokensDeposited = false; // Flag indicating the preSale tokens are deposited
+    uint256 public totalUsdtCollected = 0; 
+    mapping(address => uint256) public contributions; // Mapping of user contributions
 
     constructor(
         address _usdtToken,
@@ -34,6 +34,7 @@ contract PreSaleContract is Ownable {
         endTimestamp = _endTimestamp;
     }
 
+    // Function to contribute USDT to the funding
     function contribute(uint256 usdtAmount) public {
         require(block.timestamp >= startTimestamp && block.timestamp <= endTimestamp, "Funding is not active");
         require(tokensDeposited, "preSale tokens must be deposited first");
@@ -43,7 +44,8 @@ contract PreSaleContract is Ownable {
         totalUsdtCollected += usdtAmount;
     }
     
-    function withdrawFundsAndDistributeTokens() public onlyOwner {
+    // Function to withdraw funds to the owner
+    function withdrawFunds() public onlyOwner {
         require(fundingComplete == false, "Funds already withdrawn");
         require(block.timestamp > endTimestamp || totalUsdtCollected >= goalUsdt, "Funding period is not over or goal not reached");
         require(tokensDeposited, "preSale tokens must be deposited first");
@@ -52,6 +54,7 @@ contract PreSaleContract is Ownable {
         require(usdtToken.transfer(owner(), totalUsdtCollected), "Failed to transfer USDT to owner");
     }
 
+    // Function to claim tokens for the contribution
     function claimTokens() public {
     require(fundingComplete, "Funding not complete");
     require(contributions[msg.sender] > 0, "No contribution made");
@@ -62,18 +65,20 @@ contract PreSaleContract is Ownable {
 
     contributions[msg.sender] = 0; 
     require(preSaleToken.transfer(msg.sender, tokensToClaim), "Failed to transfer tokens");
-}
+    }
 
-
+    // Function to deposit preSale tokens to the contract
     function depositPreSaleTokens(uint256 tokenAmount) public onlyOwner {
         require(preSaleToken.transferFrom(msg.sender, address(this), tokenAmount), "Token transfer failed");
         tokensDeposited = true;
     }
 
+    // Function to get the USDT balance of the contract
     function getUsdtBalance() public view returns (uint256) {
         return usdtToken.balanceOf(address(this));
     }
 
+    // Function to get the presale token balance of the contract
     function getpreSaleTokenBalance() public view returns (uint256) {
         return preSaleToken.balanceOf(address(this));
     }
