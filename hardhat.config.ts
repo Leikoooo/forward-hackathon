1d55efbaeb0ff075fe1b0cce7e8c219bcaf4845b
Никита <solomon.www@mail.ru>
2024-05-04 18:07:46 +0300

    test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: {
