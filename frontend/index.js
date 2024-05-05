46ea6614bc358f6a7d8cd7878dcd784fd43af0c3
Никита <solomon.www@mail.ru>
2024-05-05 11:40:01 +0300

    sync

---
+++ frontend/index.js
@@ -0,0 +1,306 @@
+* {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  body{
+    font-family:Arial, sans-serif;
+    background-color:#222222;
+  }
+  .wrapper{
+    display:flex;
+    position:static;
+    float:none;
+    justify-content:center;
+  }
+  .container{
+    display:flex;
+    width:80vw;
+    justify-content:center;
+  }
+  .column1{
+    display:flex;
+    flex-direction:column;
+    width:50vw;
+    margin-right:20px;
+  }
+  .column2{
+    display:flex;
+    flex-direction:column;
+  }
+  .row{
+    background-color:#282828;
+    padding:20px 20px 24px 20px;
+    border-radius:20px 20px 20px 20px;
+    box-shadow:0px 4px 4px 0px #00000040;
+    color:#AEAEAE;
+    margin:0 0 9px 0;
+  }
+  .descriptionRow{
+    display:flex;
+    flex-direction:column;
+    background-color:#282828;
+    margin-bottom:10px;
+    padding:20px 20px 20px 20px;
+    border-radius:20px 20px 20px 20px;
+    box-shadow:0px 4px 4px 0px #00000040;
+    color:#AEAEAE;
+  }
+  .avatar{
+    width:70px;
+    height:70px;
+    border-radius:20px 20px 20px 20px;
+    overflow:hidden;
+  }
+  .companyTitle{
+    display:flex;
+  }
+  .titleContainer{
+    padding-left:10px;
+  }
+  .titleContainer p{
+    margin:5px 0 5px 0;
+  }
+  .links a{
+    color:#ffffff;
+    margin-right:10px;
+  }
+  .presaleAddress{
+    padding:10px;
+    display:flex;
+    justify-content:space-between;
+  }
+  .presaleAddress p{
+    margin:5px 0 5px 0;
+    word-wrap:break-word;
+  }
+  .tokenAddress{
+    padding:10px;
+    display:flex;
+    justify-content:space-between;
+  }
+  .tokenAddress p{
+    margin:5px 0 5px 0;
+    word-wrap:break-word;
+  }
+  .address{
+    color:#5DA040;
+  }
+  .description{
+    display:flex;
+    justify-content:space-between;
+    border-bottom:1px solid #AEAEAE29;
+  }
+  .descriptionEnd{
+    display:flex;
+    justify-content:space-between;
+  }
+  .value{
+    color:#ffffff;
+  }
+  .depositContainer p{
+    margin:5px 0 5px 0;
+    color:#ffffff;
+  }
+  .depositContainer{
+    display:flex;
+    flex-direction:column;
+    background:#282828;
+    padding:20px 20px 26px 20px;
+    border-radius:15px;
+    border:1px;
+    opacity:0px;
+    box-shadow:0px 4px 4px 0px #00000040;
+  }
+  .depositRow{
+    background-color:#2E2E2E;
+    margin-bottom:10px;
+    padding:20px 20px 20px 20px;
+    border-radius:20px 20px 20px 20px;
+    box-shadow:0px -2px 13.2px 7px #8C8C8C0F inset;
+    color:#AEAEAE;
+  }
+  .contribution{
+    font-size:0.9rem;
+    color:#585858;
+    margin:20px 0 0 20px;
+  }
+  .contribution em{
+    color:#517D3D;
+  }
+  .timeLeft{
+    display:flex;
+    justify-content:space-between;
+    gap:10px;
+    width:100%;
+  }
+  .days{
+    display:flex;
+    width:4.5rem;
+    height:3.5rem;
+    flex-direction:column;
+    align-items:center;
+    background-color:#1D1D1D;
+    border-radius:9px;
+    justify-content:center;
+  }
+  .hours{
+    display:flex;
+    width:4.5rem;
+    height:3.5rem;
+    flex-direction:column;
+    align-items:center;
+    background-color:#1D1D1D;
+    border-radius:9px;
+    justify-content:center;
+  }
+  .minutes{
+    display:flex;
+    width:4.5rem;
+    height:3.5rem;
+    flex-direction:column;
+    align-items:center;
+    background-color:#1D1D1D;
+    border-radius:9px;
+    justify-content:center;
+  }
+  .seconds{
+    display:flex;
+    width:4.5rem;
+    height:3.5rem;
+    flex-direction:column;
+    align-items:center;
+    background-color:#1D1D1D;
+    border-radius:9px;
+    justify-content:center;
+  }
+  .days p, .hours p, .minutes p, .seconds p{
+    margin:0;
+    color:#C0C0C0;
+  }
+  .days p.value, .hours p.value, .minutes p.value, .seconds p.value{
+    color:#ffffff;
+  }
+  .barContainer{
+    display:flex;
+    margin:20px 0 20px 0;
+    flex-direction:column;
+  }
+  .bar{
+    width:100%;
+    height:7px;
+    background-color:#1D1D1D;
+    border-radius:10px;
+  }
+  .progress{
+    height:100%;
+    background-color:#5DA040;
+    border-radius:10px;
+  }
+  .goal{
+    display:flex;
+    justify-content:space-between;
+    width:100%;
+  }
+  .depositContainer input{
+    padding:14px 20px 14px 20px;
+    background-color:#1D1D1D;
+    color:#ffffff;
+    border:none;
+    border-radius:10px;
+    outline:none;
+  }
+  .depositContainer button{
+    margin-top:10px;
+    padding:14px 20px 14px 20px;
+    background-color:#538C39;
+    color:#ffffff;
+    border:none;
+    border-radius:10px;
+    cursor:pointer;
+    box-shadow:0px 4px 11.6px 0px #6EBD4A inset;
+  }
+  .stages{
+    display:flex;
+    flex-direction:column;
+    padding:0 0 0 10px;
+  }
+  .stage{
+    display:flex;
+    padding:10px 0 10px 0;
+    margin:0 0 10px 0;
+    border-left:3px solid #AEAEAE29;
+    flex-direction:column;
+    padding-left:10px;
+    height:2rem;
+    justify-content:center;
+  }
+  .stage p{
+    margin:0;
+    color:#AEAEAE;
+  }
+  #id8cfh{
+    background-color:rgb(13, 108, 249);
+  }
+  #ilhzrl{
+    background-color:rgb(13, 108, 249);
+  }
+  .btn{
+    display:none;
+  }
+  
